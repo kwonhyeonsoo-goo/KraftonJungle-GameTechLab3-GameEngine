8c3cfe9e3e8fea64d9bb958f7b2651697171013c
JunHyeop3631 <sstechangle@gmail.com>
2026-03-10 11:00:07 +0900

    UTestObject에 Create 함수 추가

--- week_01/KraftonJungle3_Week1_Team5/UTestObject.h
+++ week_01/KraftonJungle3_Week1_Team5/UTestObject.h
@@ -6,6 +6,8 @@ class USphereMesh;
 class UTestObject : public UGameObject
 {
 public:
+	void Create(ID3D11Device* device, ID3D11DeviceContext* context);
+
 	void Physics_Update(float tick) override;
 	void Update(float tick) override;
 	void Render(ID3D11DeviceContext* context, ID3D11Device* device) override;
