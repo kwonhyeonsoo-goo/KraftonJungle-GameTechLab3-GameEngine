a220f132f376e66c1c928a2a50cfa35b27af82d7
강명호 <3044965@gmail.com>
2026-03-10 10:04:04 +0900

    MainGameScene 추가

---
+++ week_01/KraftonJungle3_Week1_Team5/UMainGameScene.cpp
@@ -0,0 +1,20 @@
+#include "UMainGameScene.h"
+
+#include "SceneAutoRegister.h"
+
+REGISTER_SCENE(UMainGameScene)
+
+void UMainGameScene::Initialize(ID3D11Device* device, ID3D11DeviceContext* context)
+{
+
+}
+
+void UMainGameScene::Update(float tick)
+{
+
+}
+
+void UMainGameScene::Exit()
+{
+
+}
---
+++ week_01/KraftonJungle3_Week1_Team5/UMainGameScene.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "UScene.h"
+class UMainGameScene : public UScene
+{
+public:
+	void Initialize(ID3D11Device* device, ID3D11DeviceContext* context) override;
+	void Update(float tick) override;
+	void Exit() override;
+};
+
