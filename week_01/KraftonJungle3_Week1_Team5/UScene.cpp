746928fef900ee92a13e236f3e3ccd6cdc6c3357
keonwookang0914 <kwkang0914@gmail.com>
2026-03-10 18:50:31 +0900

    feature: CheckCollision 함수 추가 & 오브젝트 초기화 샘플 일부 수정

--- week_01/KraftonJungle3_Week1_Team5/UScene.cpp
+++ week_01/KraftonJungle3_Week1_Team5/UScene.cpp
@@ -2,6 +2,9 @@
 
 #include "Utility.h"
 #include "UGameObject.h"
+#include "UCollider.h"
+#include "UCircleCollider.h"
+#include "URectCollider.h"
 
 UScene::~UScene()
 {
@@ -21,5 +24,67 @@ void UScene::Release()
 	for (auto& gameObject : GameObjects)
 	{
 		SafeReleaseAndDelete(gameObject);
+	}
+}
+
+void UScene::CheckCollision()
+{
+	for (int i = 0; i < GameObjects.size(); ++i)
+	{
+		for (int j = i + 1; j < GameObjects.size(); ++j)
+		{
+			ColliderType type1 = GameObjects[i]->GetCollider()->GetColliderType();
+			ColliderType type2 = GameObjects[j]->GetCollider()->GetColliderType();
+			if (type1 == ColliderType::ColliderType_Circle)
+			{
+				// 당장은 쓸 일 없음
+				if (type1 == type2) //circle circle
+				{
+					// 구끼리 충돌 체크
+					UCircleCollider* CircleCollider1 = static_cast<UCircleCollider*>(GameObjects[i]->GetCollider());
+					UCircleCollider* CircleCollider2 = static_cast<UCircleCollider*>(GameObjects[j]->GetCollider());
+					bool bResult = CircleCollider1->CheckCollisionCC(CircleCollider2);
+					if (bResult)
+					{
+						// Hit: TODO: 반발력 발생
+					}
+				}
+				else //circle - rect
+				{
+					// 원 - 박스 충돌 체크
+					UCircleCollider* CircleCollider = static_cast<UCircleCollider*>(GameObjects[i]->GetCollider());
+					URectCollider* RectCollider = static_cast<URectCollider*>(GameObjects[j]->GetCollider());
+					bool bResult = CircleCollider->CheckCollisionCR(RectCollider);
+					if (bResult)
+					{
+						// Hit: TODO: 반발력 발생
+					}
+				}
+			}
+			else //ColliderType_Rect
+			{
+				if (type1 == type2)
+				{
+					URectCollider* RectCollider1 = static_cast<URectCollider*>(GameObjects[i]->GetCollider());
+					URectCollider* RectCollilder2 = static_cast<URectCollider*>(GameObjects[j]->GetCollider());
+					bool bResult = RectCollider1->CheckCollisionRR(RectCollilder2);
+					if (bResult)
+					{
+						// Hit: TODO: 반발력 발생
+					}
+				}
+				else //rect circle
+				{
+					// 원과 박스의 충돌 체크
+					URectCollider* RectCollider = static_cast<URectCollider*>(GameObjects[i]->GetCollider());
+					UCircleCollider* CircleCollider = static_cast<UCircleCollider*>(GameObjects[j]->GetCollider());
+					bool bResult = CircleCollider->CheckCollisionCR(RectCollider);
+					if (bResult)
+					{
+						// Hit: TODO: 반발력 발생
+					}
+				}
+			}
+		}
 	}
 }
