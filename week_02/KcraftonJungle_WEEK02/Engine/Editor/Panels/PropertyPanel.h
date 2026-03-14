292a7fdeae4e5269f627b410b4adc4a4216a93c1
Chanil-Chong <chan450471@gmail.com>
2026-03-14 21:18:17 +0900

    Add PropertyPanel

---
+++ week_02/KcraftonJungle_WEEK02/Engine/Editor/Panels/PropertyPanel.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+#include "IEditorPanel.h"
+#include "../Commands/SetTransformCommand.h"
+#include "../../../AppContext.h"
+#include "../../../Editor/ImGui/imgui.h"
+
+class PropertyPanel : public IEditorPanel {
+public:
+    void OnRender(AppContext& ctx) override;
+    FString GetName() const override { return "Property"; }
+
+    void OnObjectDestroyed(const ObjectDestroyedEvent& e);
+
+    void OnSelectionChanged(const SelectionChangedEvent& e);
+
+private:
+    const USceneComponent* Current = nullptr;
+
+    DelegateHandle SelectionChangedHandle = 0;
+    DelegateHandle ObjectDestroyedHandle = 0;
+};
