10d5e7d852e0639d2729399906cb8bcd2b056dfb
kwonhyeonsoo-goo <kkhhss4035@gmail.com>
2026-03-15 15:06:14 +0900

    3.15 외곽선 thickness 보정

--- week_02/KcraftonJungle_WEEK02/Engine/Rendering/URenderer.cpp
+++ week_02/KcraftonJungle_WEEK02/Engine/Rendering/URenderer.cpp
@@ -693,10 +693,12 @@ void URenderer::Flush(const RenderQueue& queue, const EditorSession& session)//,
             break;
         case ERenderType::Highlight:
         {
+            FVector direction = session.Camera.Position - FVector(constants.MVP.M[3][0], constants.MVP.M[3][1], constants.MVP.M[3][3]);
+            float distance = sqrt(direction.x * direction.x + direction.y * direction.y + direction.z * direction.z);
             DeviceContext->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
 
             PrepareOutlineShader();
-            UpdateMVP(constants.MVP, 1.1f);
+            UpdateMVP(constants.MVP, 1.0f * distance * 0.01f);
             switch (cmd.Shape)
             {
             case EPrimitiveShape::Sphere:
