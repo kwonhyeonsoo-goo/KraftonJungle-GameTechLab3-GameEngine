5de778f70093d3fe91ccd918d6de5e3912d4bd86
jskim-research <wltnwkd21@gmail.com>
2026-04-25 16:45:05 +0900

    Chore: add shadow architecture base

---
+++ week_08/NipsEngine/Source/Engine/Render/Resource/ShadowResourcePool.h
@@ -0,0 +1,28 @@
+﻿#pragma once
+#include "ShadowResource.h"
+#include "Render/Common/ShadowTypes.h"
+
+struct FShadowRequestDesc
+{
+    uint32 Resolution;
+    uint32 CascadeCount; // CSM only
+
+    // 동일한 설정의 Pool 에서 가져오기 위한 정보들
+    EShadowMapType MapType;
+    EShadowAllocationMode AllocationMode;
+};
+
+class IShadowResourcePool
+{
+public:
+    virtual ~IShadowResourcePool() = default;
+
+    // 요청 기반 리소스 확보
+    virtual FShadowResource* Acquire(const FShadowRequestDesc& desc) = 0;
+
+    // 사용 종료 (옵션 or 프레임 기반이면 생략 가능)
+    virtual void Release(FShadowResource* resource) = 0;
+
+    // 프레임 시작 초기화 (bInUse 리셋)
+    virtual void BeginFrame() = 0;
+};
---
+++ week_08/NipsEngine/Source/Engine/Render/Scene/ShadowLightSelector.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+#include "Core/Common.h"
+#include "Component/Light/LightComponent.h"
+#include "Render/Common/ShadowTypes.h"
+
+
+struct FShadowRequest
+{
+    uint32 LightId;      // 어떤 라이트인지
+    ELightType Type; // Directional / Point / Spot
+
+    int Resolution; // Shadow Map 해상도
+    uint32 CascadeCount;
+
+    bool bUseVSM; // VSM 사용할지 여부
+
+    EShadowProjectionMode ProjectionMode = EShadowProjectionMode::Default;
+};
+
+class IShadowLightSelector
+{
+public:
+    virtual std::vector<FShadowRequest>
+    SelectShadowLights(const Scene& scene, const Camera& camera) = 0;
+};
